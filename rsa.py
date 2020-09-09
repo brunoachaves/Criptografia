ad4f0c632ea48ac9d4b3e9e6d4b597fd8fa80838
Bruno de Araujo Chaves <achaves.bruno@gmail.com>
2020-09-08 21:31:02 -0300

    Adding encrypt and decrypt steps in rsa.py

--- rsa.py
+++ rsa.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python
+from functools import reduce
 import numpy as np
-from math import log10
 import os
+import time
 
 
-def print_hex_list(int_list):
-    """Print ciphertext in hex"""
-    for index, elem in enumerate(int_list):
-        print("{:02x}".format(elem), end="")
-    print()
-
-
-def print_large_integer(number):
-    """Print long primes in a formatted way"""
-    string = "{:02x}".format(number)
+# Organizando o print de um número grande, convertendo-o para hexadecimal
+def print_large_number(large_number):
+    string = "{:02x}".format(large_number)
     for j in range(len(string)):
+        if j % 64 == 0 and j > 0:
+            print()
         print(string[j], end="")
     print()
 
@@ -30,6 +26,7 @@ def get_primes(min_value, max_value):
     return prime_1, prime_2, prime_1_2
 
 
+# Método para auxiliar no cálculo do inverso multiplicativo modular
 def egcd(a, b):
     if a == 0:
         return b, 0, 1
@@ -38,45 +35,57 @@ def egcd(a, b):
         return g, x - (b // a) * y, y
 
 
+# Calculando o inverso multiplicativo modular
 def mod_inv(a, m):
     g, x, y = egcd(a, m)
     if g != 1:
-        raise Exception('modular inverse does not exist')
+        raise Exception('[Erro] inverso multiplicativo modular não existe!')
     else:
         return x % m
 
 
 if __name__ == '__main__':
+    # Recebendo o texto de entrada
     data_in = input('Digite o texto de entrada:\n')
+    begin = time.time()
     bit_length = 4096
 
+    # Gerar os números primos adequados
     n_min = 1 << (bit_length - 1)
     n_max = (1 << bit_length) - 1
     p, q, n = get_primes(n_min, n_max)
     print(f'\nTamanho da chave = {len(bin(n)[2:])} bits')
     print('\nPrimeiro número primo:')
-    print_large_integer(p)
+    print_large_number(p)
     print('\nSegundo número primo:')
-    print_large_integer(q)
+    print_large_number(q)
 
+    # Calculando a função totiente phi(N)
     phi = (p - 1) * (q - 1)
+    # Calculando o 'e'
     e = 65635
     while np.gcd(e, phi) > 1:
         e += 2
+    # Calculando o módulo inverso multiplicativo
     d = mod_inv(e, phi)
 
-    # print('\nFunção totiente:')
-    # print_large_integer(phi)
-    # print('\ne:')
-    # print(e)
-    # print(f'\nd:')
-    # print_large_integer(d)
-
-    encrypted = pow(ord(data_in), e, n)
-    decrypted = pow(encrypted, d, n)
     print('\nDado original:')
     print(data_in)
+
+    # Encriptando o texto
+    nbytes = min(len(data_in), bit_length - 1)
+    data_int = reduce(lambda x, y: (x << 8) + y, map(ord, data_in[:bit_length]))
+    encrypted_int = pow(data_int, e, n)
+    encrypted_list = [(encrypted_int >> j) & 0xff for j in reversed(range(0, bit_length, 8))]
     print('\nDado encriptado:')
-    print_large_integer(encrypted)
+    print_large_number(encrypted_int)
+
+    # Decriptando o texto
+    decrypted_list = reduce(lambda x, y: (x << 8) + y, encrypted_list[:bit_length + 2])
+    decrypted_int = pow(decrypted_list, d, n)
+    decrypted = "".join([chr((decrypted_int >> j) & 0xff) for j in reversed(range(0, bit_length << 3, 8))])
     print('\nDado decriptado:')
-    print(chr(decrypted))
+    print(decrypted)
+
+    # Tempo de execução
+    print(f'\nTempo de execução: {round(time.time() - begin, 2)}')
